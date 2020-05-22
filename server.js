a2a53ce7cfd6c555a0db9750cb80d8eb4affce70
TheMatthewSawyer <matthewleonardsawyer@gmail.com>
2020-05-22 05:07:28 -0400

    More small changes.

--- server.js
+++ server.js
@@ -4,13 +4,9 @@ const port = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "client/build", "index.html")));
 
-  app.listen(port);
-  console.log(`app listening on ${port}`);
+app.listen(port, () => console.log(`Server listening on port ${port}`));
